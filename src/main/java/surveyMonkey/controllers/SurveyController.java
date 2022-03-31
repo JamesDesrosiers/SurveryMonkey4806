a8ebb1f00315f41c9abfb92262c63d606c24be40
Hazel Griffith <hazelgriffith30@gmail.com>
2022-03-31 17:02:02 -0400

    Added canvasjs

--- src/main/java/surveyMonkey/controllers/SurveyController.java
+++ src/main/java/surveyMonkey/controllers/SurveyController.java
@@ -1,17 +1,15 @@
 package surveyMonkey.controllers;
 
-import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import surveyMonkey.models.Question;
+import surveyMonkey.models.CanvasjsChartData;
 import surveyMonkey.models.Response;
 import surveyMonkey.models.Survey;
 import surveyMonkey.services.FirebaseInitializer;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -42,7 +40,11 @@ public class SurveyController {
     }
 
     @RequestMapping("/surveys")
-    public ModelAndView resultsPage(@RequestParam(value="id", required=true) String id, @ModelAttribute("survey") Survey survey) throws ExecutionException, InterruptedException {
+    public ModelAndView resultsPage(@RequestParam(value="id", required=true) String id, @ModelAttribute("survey") Survey survey, ModelMap modelMap) throws ExecutionException, InterruptedException {
+        List<List<Map<Object, Object>>> canvasjsDataList = CanvasjsChartData.getCanvasjsDataList();
+        modelMap.addAttribute("dataPointsList", canvasjsDataList);
+
+
         DocumentReference query = db.getFirebase().document("surveys/"+id);
 
         survey.setSurvey(query.get().get());
